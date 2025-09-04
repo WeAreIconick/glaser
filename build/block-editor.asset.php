9dfa6198f0fc5d02ef129fed89063501452aa134
George Stephanis <daljo628@gmail.com>
2025-09-04 10:40:32 -0400

    Set up arrow block style.  Get hover state running.

--- build/block-editor.asset.php
+++ build/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-i18n'), 'version' => 'f62d36888b4a6d311130');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-i18n'), 'version' => '90b10a5ef6a836741705');
--- src/block-editor.jsx
+++ src/block-editor.jsx
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import domReady from '@wordpress/dom-ready';
-import registerBlockStyle from '@wordpress/blocks';
+import { registerBlockStyle } from '@wordpress/blocks';
 
 domReady(() => {
 	registerBlockStyle('core/button', {
