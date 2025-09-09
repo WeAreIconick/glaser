7e15727dec0012bfcfa2bffc14871c022c180231
George Stephanis <daljo628@gmail.com>
2025-09-09 13:06:44 -0400

    Create partners-logos.php

---
+++ patterns/partners-logos.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: Partners Logos
+ * Slug: glaser/partners-logos
+ * Categories: Media, Portfolio, Services
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Partners Group"},"align":"full","style":{"border":{"width":"1px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="border-width:1px"><!-- wp:heading {"level":3,"align":"full","style":{"border":{"bottom":{"width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}}} -->
+<h3 class="wp-block-heading alignfull" style="border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><?php esc_html_e( 'Partners', 'glaser' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:group {"align":"full","className":"partners-grid","layout":{"type":"grid","columnCount":4,"minimumColumnWidth":null}} -->
+<div class="wp-block-group alignfull partners-grid"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
